c25ef8db992e2f0c738330ac88a0bdfbfc557f33
noah1216 <76505928+noah1216@users.noreply.github.com>
2021-01-19 20:55:11 +0900

    userモデルの単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i}
   validates :nickname, presence: true
   validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
