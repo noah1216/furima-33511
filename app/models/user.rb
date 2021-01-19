805005ee42efecd91f45a24d6e9ba81ee9cc407d
noah1216 <76505928+noah1216@users.noreply.github.com>
2021-01-19 17:08:15 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :email, presence: true, format: { with: /\A\S+@\S+\.\S+\z/}
-  validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A[a-z0-9]+\z/}
   validates :nickname, presence: true
   validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
