8c6ddc05ec9d7e4285731d8ebb1cb733b543cf04
noah1216 <76505928+noah1216@users.noreply.github.com>
2021-01-20 19:05:22 +0900

    手順3 ActiveHashを導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,18 @@
 class Item < ApplicationRecord
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :state
+  belongs_to :shopping_charge
+  belongs_to :area
+  belongs_to :shopping_day
+
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :state_id
+    validates :shopping_charge
+    validates :area
+    validates :shopping_day
+  end
 end
---
+++ app/models/shopping_charge.rb
@@ -0,0 +1,2 @@
+class ShoppingCharge < ApplicationRecord
+end
